b515dc6a55e0907e6e9f048a813f935ebd6b422c
albertrein <guilhermealberto8@gmail.com>
2020-05-11 00:11:17 -0300

    Inserindo pergunta aleatória listando pelo banco e atualizando script de listagem.

--- backend/listarperguntas.php
+++ backend/listarperguntas.php
@@ -1,38 +1,16 @@
 <?php
     include_once('dbconnection.php');
-
-    /*
-     * Recebe a palavra all via get
-     */
-
-    $err = FALSE;
-    (isset($_GET['perguntas']) and !empty($_GET['perguntas'])) ? $perguntas = $_GET['perguntas'] : $err = TRUE;
-
-    if (($perguntas != "all") || ($err)) {
-        mysqli_close($db_conn);	
-	    exit;
-    }
-
-    $query = 'SELECT  P.id, P.pergunta, P.resposta FROM MEMORY_MATH.PERGUNTA P
-                                                   ORDER BY P.ID';
+    $query = "SELECT PERGUNTA, RESPOSTA FROM pergunta ORDER BY RAND() LIMIT 1";
 
     $result = mysqli_query($db_conn, $query);
-    $qtd = mysqli_num_rows($result);
-
-    if ($qtd > 0) {
-        while ($row = mysqli_fetch_assoc($result)) {
-            $rows[] = $row;
-        }
-    } else {
-        mysqli_close($db_conn);	
-	    exit;
-    }
-
-    $json_str = json_encode($rows);
-    echo "$json_str";
-
-    mysqli_query($db_conn, $query) or die(mysqli_error());
     
+    if (!$result || mysqli_num_rows($result) <= 0) {
+        mysqli_close($db_conn);
+        echo json_encode([]);
+        exit;
+    }
+    $row = mysqli_fetch_assoc($result);
+    echo json_encode($row);
 	mysqli_close($db_conn);	
 	exit;
 ?>
