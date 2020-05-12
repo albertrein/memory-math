6bd432e09396d1e519867c03db993734f7b99320
albertrein <guilhermealberto8@gmail.com>
2020-05-12 13:31:59 -0300

    Inserindo atualização dos pontos

--- backend/atualizarpontos.php
+++ backend/atualizarpontos.php
@@ -10,18 +10,15 @@
     $nome = $json->nome;
     $pontos = $json->pontos;
     
-    $query = 'SELECT J.pontos FROM MEMORY_MATH.JOGADOR J WHERE J.NOME="'.$nome.'"';
+    $query = 'SELECT pontos FROM jogador WHERE NOME="'.$nome.'"';
     $result = mysqli_query($db_conn, $query);
     $row = mysqli_fetch_assoc($result);
 
     if ($row['pontos'] < $pontos) {
-        $query = 'UPDATE MEMORY_MATH.JOGADOR SET PONTOS='.$pontos.' WHERE NOME="'.$nome.'"';
+        $query = 'UPDATE jogador SET PONTOS='.$pontos.' WHERE NOME="'.$nome.'"';
         mysqli_query($db_conn, $query) or die(mysqli_error());
-    } else {
-        mysqli_close($db_conn);	
-        exit;
     }
-
+    
 	mysqli_close($db_conn);	
 	exit;
 ?>
