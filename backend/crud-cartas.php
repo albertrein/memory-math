b805c0647c997c710f7ad34cd68f2dc75eed0f34
albertrein <guilhermealberto8@gmail.com>
2020-05-13 23:57:51 -0300

    Inserindo update.

--- backend/crud-cartas.php
+++ backend/crud-cartas.php
@@ -48,9 +48,38 @@
         }
     }
 
+    function update($db, $cod, $campo1, $campo2){
+        $query = "UPDATE carta_calculo CC, carta_resposta CR SET CC.expressao='".$campo1."', CR.resposta='".$campo2."' WHERE CC.resposta = CR.id AND CC.id=".$cod;
+        $result = mysqli_query($db, $query)or die(mysqli_error($db));
+        $row = mysqli_fetch_assoc($result);
+
+        if($row['total'] == 0) {
+            cadastrarCartaCalculo($db, $campo1, $campo2);
+            echo json_encode("Ok");
+        } else {
+            echo "null";
+        }
+    }
+
     $json = json_decode(file_get_contents('php://input'));
-    
-    insert($db_conn,"9 * 10","90");
+    $cod = $json->cod;
+    $acao = $json->acao;
+    $campo1 = $json->campo1;
+    $campo1 = $json->campo2;
+
+    switch($acao){
+        case "insert":
+            insert($db_conn, $campo1, $campo2);    
+            break;
+        case "delete":
+            insert($db_conn, $cod);
+        break;
+        case "update":
+            update($db_conn, $cod, $campo1, $campo2);
+            break;
+        default:
+            select($db_conn);
+    }
     
 	mysqli_close($db_conn);	
 	exit;
