5f9afdfe06dd4e647b7cdb6f19f9ca9b9e825ba2
albertrein <guilhermealberto8@gmail.com>
2020-05-13 23:14:48 -0300

    Arrumando carta functions.

--- backend/cadastrarcartafunctions.php
+++ backend/cadastrarcartafunctions.php
@@ -2,7 +2,7 @@
 
     function cadastrarCartaResposta($db_conn, $resultado) {
         
-        $query = 'INSERT INTO CARTA_RESPOSTA(RESULTADO) VALUES ('.$resultado.')';
+        $query = 'INSERT INTO carta_resposta(RESULTADO) VALUES ('.$resultado.')';
         mysqli_query($db_conn, $query) or die(mysqli_error());
 
         return true;
@@ -10,22 +10,22 @@
 
     function cadastrarCartaCalculo($db_conn, $expressao, $resultado) {
         
-        $query = 'SELECT CR.ID FROM MEMORY_MATH.CARTA_RESPOSTA CR WHERE CR.RESULTADO = '.$resultado.'';
+        $query = 'SELECT CR.ID FROM carta_resposta CR WHERE CR.RESULTADO = '.$resultado.'';
         $result = mysqli_query($db_conn, $query);
         $qtd = mysqli_num_rows($result);
 
         if ($qtd > 0) {
             $row = mysqli_fetch_assoc($result);
-            $query = 'INSERT INTO CARTA_CALCULO(EXPRESSAO,RESPOSTA) VALUES ("'.$expressao.'",'.$row['ID'].')';
+            $query = 'INSERT INTO carta_calculo(EXPRESSAO,RESPOSTA) VALUES ("'.$expressao.'",'.$row['ID'].')';
             mysqli_query($db_conn, $query) or die(mysqli_error());
         } else {
             cadastrarCartaResposta($db_conn, $resultado);
 
-            $query = 'SELECT CR.ID FROM MEMORY_MATH.CARTA_RESPOSTA CR WHERE CR.RESULTADO = '.$resultado.'';
+            $query = 'SELECT CR.ID FROM carta_resposta CR WHERE CR.RESULTADO = '.$resultado.'';
             $result = mysqli_query($db_conn, $query);
             $row = mysqli_fetch_assoc($result);
             
-            $query = 'INSERT INTO CARTA_CALCULO(EXPRESSAO,RESPOSTA) VALUES ("'.$expressao.'",'.$row['ID'].')';
+            $query = 'INSERT INTO carta_calculo(EXPRESSAO,RESPOSTA) VALUES ("'.$expressao.'",'.$row['ID'].')';
             mysqli_query($db_conn, $query) or die(mysqli_error());
         }
 
