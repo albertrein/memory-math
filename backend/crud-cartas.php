0027e87bed79f4ce7a65d9ab5ff3b17a4c600cab
albertrein <guilhermealberto8@gmail.com>
2020-05-15 20:18:56 -0300

    Finalizando crud cartas.

--- backend/crud-cartas.php
+++ backend/crud-cartas.php
@@ -21,14 +21,12 @@
      }
 
     function delete($db,$cod){
-
-        $query = 'DELETE CC, CR FROM carta_calculo CC JOIN carta_resposta CR WHERE CC.resposta = CR.id AND CC.id='.$cod;
-        $result = mysqli_query($db, $query);
-
-        if($result && mysqli_num_rows($result) > 0){
-            while ($row = mysqli_fetch_assoc($result)) {
-                $rows[] = $row;
-            }
+        $query = "SET FOREIGN_KEY_CHECKS=OFF"; //Gambi pra fazer o delete
+        mysqli_query($db, $query) or die(json_encode('null'));
+        $query = 'DELETE carta_calculo, carta_resposta FROM carta_calculo INNER JOIN carta_resposta WHERE carta_calculo.RESPOSTA=carta_resposta.ID AND carta_calculo.ID='.$cod;
+        $result = mysqli_query($db, $query) or die(json_encode('null'));
+        //echo $query."-";
+        if($result){
             echo json_encode("Ok!");
             return true;
         }
@@ -37,7 +35,7 @@
 
     function insert($db,$campo1, $campo2){
         $query = 'SELECT COUNT(*) AS total FROM carta_calculo CC WHERE CC.expressao = "'.$campo1.'"';
-        $result = mysqli_query($db, $query)or die(mysqli_error($db));
+        $result = mysqli_query($db, $query)or die(json_encode('null'));
         $row = mysqli_fetch_assoc($result);
 
         if($row['total'] == 0) {
@@ -49,30 +47,25 @@
     }
 
     function update($db, $cod, $campo1, $campo2){
-        $query = "UPDATE carta_calculo CC, carta_resposta CR SET CC.expressao='".$campo1."', CR.resposta='".$campo2."' WHERE CC.resposta = CR.id AND CC.id=".$cod;
-        $result = mysqli_query($db, $query)or die(mysqli_error($db));
-        $row = mysqli_fetch_assoc($result);
-
-        if($row['total'] == 0) {
-            cadastrarCartaCalculo($db, $campo1, $campo2);
-            echo json_encode("Ok");
-        } else {
-            echo "null";
-        }
+        $query = "UPDATE carta_calculo CC, carta_resposta CR SET CC.expressao='".$campo1."', CR.resultado='".$campo2."' WHERE CC.resposta=CR.id AND CC.id=".$cod;
+        $result = mysqli_query($db, $query)or die(json_encode("null"));
+    
+        echo json_encode("Ok");
+        
     }
 
     $json = json_decode(file_get_contents('php://input'));
     $cod = $json->cod;
     $acao = $json->acao;
     $campo1 = $json->campo1;
-    $campo1 = $json->campo2;
+    $campo2 = $json->campo2;
 
+    //die( json_encode($acao.'-'.$cod.'-'.$campo1.'-'.$campo2));
     
-
     switch($acao){
         case "inserir":
             insert($db_conn, $campo1, $campo2);
-            break;
+        break;
         case "deletar":
             delete($db_conn, $cod);
         break;
