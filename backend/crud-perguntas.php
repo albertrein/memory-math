9b04f4935eaa8562bfcadf8d4822747dec6a71a9
albertrein <guilhermealberto8@gmail.com>
2020-05-15 22:44:59 -0300

    Inserindo manter perguntas.

---
+++ backend/crud-perguntas.php
@@ -0,0 +1,79 @@
+<?php
+    include_once('dbconnection.php');
+    include_once('cadastrarcartafunctions.php');
+
+    /*
+     * {cod: '', acao: '', campo1: '', campo2: ''}
+     */
+
+     function select($db){
+        $query = 'SELECT  id, pergunta, resposta FROM pergunta ORDER BY id ASC';
+        $result = mysqli_query($db, $query) or die(json_encode('null'));
+
+        if($result && mysqli_num_rows($result) > 0){
+            while ($row = mysqli_fetch_assoc($result)) {
+                $rows[] = $row;
+            }
+            echo json_encode($rows);
+            return true;
+        }
+        echo "null";
+     }
+
+    function delete($db,$cod){
+        $query = 'DELETE FROM pergunta WHERE pergunta.id='.$cod;
+        $result = mysqli_query($db, $query) or die(json_encode('null'));
+        if($result){
+            echo json_encode("Ok!");
+            return true;
+        }
+        echo "null";
+    }
+
+    function insert($db,$campo1, $campo2){
+        $query = 'SELECT COUNT(*) AS total FROM pergunta WHERE pergunta.pergunta="'.$campo1.'"';
+        $result = mysqli_query($db, $query)or die(json_encode('null'));
+        $row = mysqli_fetch_assoc($result);
+
+        if($row['total'] == 0) {
+            $query = 'INSERT INTO pergunta(PERGUNTA, RESPOSTA) VALUE ("'.$campo1.'", "'.$campo2.'" )';
+            $result = mysqli_query($db, $query)or die(json_encode('null'));
+            echo json_encode("Ok");
+        } else {
+            echo "null";
+        }
+    }
+
+    function update($db, $cod, $campo1, $campo2){
+        $query = "UPDATE pergunta P SET P.pergunta='".$campo1."', P.resposta='".$campo2."' WHERE P.id=".$cod;
+        $result = mysqli_query($db, $query)or die(json_encode("null"));
+    
+        echo json_encode("Ok");
+        
+    }
+
+    $json = json_decode(file_get_contents('php://input'));
+    $cod = $json->cod;
+    $acao = $json->acao;
+    $campo1 = $json->campo1;
+    $campo2 = $json->campo2;
+
+    //die( json_encode($acao.'-'.$cod.'-'.$campo1.'-'.$campo2));
+    
+    switch($acao){
+        case "inserir":
+            insert($db_conn, $campo1, $campo2);
+        break;
+        case "deletar":
+            delete($db_conn, $cod);
+        break;
+        case "atualizar":
+            update($db_conn, $cod, $campo1, $campo2);
+            break;
+        default:
+            select($db_conn);
+    }
+    
+	mysqli_close($db_conn);	
+	exit;
+?>
