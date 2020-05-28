31469c7cb1580b161ae67dfff8c4a0a0d4a987dc
albertrein <guilhermealberto8@gmail.com>
2020-05-27 22:25:29 -0300

    Removinf trash files.

--- backend/dbconnection.php
+++ backend/dbconnection.php
@@ -1,7 +1,7 @@
 <?php
-    define('DB_SERVIDOR', 'localhost');
+    define('DB_SERVIDOR', 'db');
     define('DB_USUARIO', 'root');
-    define('DB_SENHA', '');
+    define('DB_SENHA', 'phprs');
     define('DB_BANCO', 'memory_math');
 
     $db_conn = mysqli_connect(DB_SERVIDOR, DB_USUARIO, DB_SENHA, DB_BANCO) or die('Não foi possivel conectar ao banco de dados.'); 
